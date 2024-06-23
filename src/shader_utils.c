f6cb8a5ba9d2731d83fee3b537354f7a7023065a
Constantin Nicula <constantin.nicula97@gmail.com>
2024-06-24 01:18:07 +0300

    Implemented shader loading

--- src/shader_utils.c
+++ src/shader_utils.c
@@ -1,10 +1,78 @@
 #include "shader_utils.h"
-#include "glib.h"
+#include "gst/gstplugin.h"
 #include "log_utils.h"
+#include "hmap.h"
+
+#include <complex.h>
+#include <linux/videodev2.h>
 #include <stdlib.h>
+#include <dirent.h>
 #include <stdio.h>
 
-char* load_shader(const char* shader_path) {
+static int is_shader(const char* file_name, char** out_shader_name); 
+static char* path_join(const char* file_part1, const char* file_part2); 
+static char* load_shader_code(const char* shader_path);
+
+/* Hash map which will store all loaded shader (mainly used for deduplication)
+   Must be initialized via a call to init_shader_store();
+*/
+static HashMap_t* shader_store = NULL;
+
+int init_shader_store() { 
+    shader_store = create_hash_map();
+    CHECK(shader_store != NULL, "Failed to create shader store", RET_ERR);
+    return RET_OK;
+}
+
+int add_shaders_to_store(const char* shader_folder_path) {
+    DIR *dir = NULL;
+    struct dirent *dir_entry = NULL;
+
+    dir = opendir(shader_folder_path);
+    if (!dir) {
+        ERROR_FMT("Failed to open shader folder %s\n", shader_folder_path);
+        return RET_ERR;
+    }
+
+    /* Load each shader and store the contents in the shader hmap.*/
+    while ((dir_entry = readdir(dir)) != NULL) {
+        char* shader_name; 
+        if (is_shader(dir_entry->d_name, &shader_name) != RET_ERR) {
+            /* Compute full path. */
+            char *full_path = path_join(shader_folder_path, dir_entry->d_name);
+            DEBUG_PRINT_FMT("Found shader [%s] at path: %s\n",shader_name, full_path);
+
+            /* Load shader code and add to store. */
+            char *shader_code = load_shader_code(full_path); 
+            if (shader_code) 
+                hash_map_insert(shader_store, shader_name, shader_code);
+            
+            /* Free malloc'd data, hashmap dups keys by default*/
+            free(shader_name);
+            free(full_path);
+        }
+        is_shader(dir_entry->d_name, NULL);
+    }
+
+    closedir(dir);
+    return RET_OK;
+}
+
+const char* get_shader_code(const char* shader_name) {
+    return hash_map_get(shader_store, shader_name);
+}
+
+
+static void _free_shader_code(void** elem) {
+    if (elem) free(*elem);
+}
+
+void cleanup_shader_store() {
+    cleanup_hash_map_elements(shader_store, _free_shader_code);
+}
+
+
+static char* load_shader_code(const char* shader_path) {
     char* buf = NULL;
     size_t file_size = 0;
 
@@ -30,4 +98,25 @@ char* load_shader(const char* shader_path) {
     fclose(file);
     
     return buf;
+}
+
+
+#define SHADER_EXT ".glsl"
+static int is_shader(const char* file_name, char** out_shader_name) {
+    size_t len = strlen(file_name);
+    size_t len_ext = strlen(SHADER_EXT);
+    if ((len <= len_ext) || (strcmp(file_name + len - len_ext, SHADER_EXT) != 0)) 
+        return RET_ERR;
+    if (out_shader_name != NULL)
+        *out_shader_name = strndup(file_name, len - len_ext);
+    return RET_OK;
+}
+
+static char* path_join(const char* file_part1, const char* file_part2) {
+    size_t out_len = strlen(file_part1) + strlen(file_part2) + 2;
+    char* ret = malloc(out_len); *ret = '\0'; 
+    strcat(ret, file_part1);
+    strcat(ret, "/");
+    strcat(ret, file_part2); 
+    return ret;
 }
--- src/shader_utils.h
+++ src/shader_utils.h
@@ -1,6 +1,9 @@
 #ifndef __SHADER_UTILS_H__
 #define __SHADER_UTILS_H__
 
-char* load_shader(const char* shader_path);
+int init_shader_store();
+int add_shaders_to_store(const char* shader_folder_path);
+const char* get_shader_code(const char* shader_name);
+void cleanup_shader_store();
 
 #endif
